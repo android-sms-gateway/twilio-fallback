c74482af2b99a39f875b1667dddf5745d5b301c7
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-06-03 10:15:00 +0700

    [proxy] introduce proxy module

--- internal/server/module.go
+++ internal/server/module.go
@@ -11,14 +11,18 @@ import (
 var Module = fx.Module(
 	"server",
 	fx.Decorate(func(log *zap.Logger) *zap.Logger {
-		return log.Named("api")
+		return log.Named("server")
 	}),
 	fx.Provide(http.NewJSONErrorHandler),
 	fx.Provide(func(log *zap.Logger) http.Options {
 		return *(&http.Options{}).WithErrorHandler(http.NewJSONErrorHandler(log))
 	}),
 	fx.Provide(handlers.NewHealthHandler, fx.Private),
-	fx.Invoke(func(app *fiber.App, hh *handlers.HealthHandler) {
+	fx.Provide(handlers.NewTwilioHandler, fx.Private),
+	fx.Invoke(func(app *fiber.App, hh *handlers.HealthHandler, th *handlers.TwilioHandler) {
 		hh.Register(app.Group("/health"))
+
+		api := app.Group("/api")
+		th.Register(api.Group("/twilio"))
 	}),
 )
